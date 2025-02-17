719d0e1f376e1283335d7cd5f9d8d40a52748ffd
emmanuel.yohore@ynov.com <emmanuel.yohore@ynov.com>
2025-02-17 13:46:15 +0100

    attribut routing test

--- symfony_Final/src/Controller/HomeController.php
+++ symfony_Final/src/Controller/HomeController.php
@@ -2,21 +2,31 @@
 
 namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController {
-    public function bonjour()
-    {
-        return new Response("Hello word");
-    }
+    // public function bonjour()
+    // {
+    //     return new Response("Hello word");
+    // }
 
-    public function aurevoir()
-    {
-        return $this->redirectToRoute('acceuil');
-    }
+    // public function aurevoir()
+    // {
+    //     return $this->redirectToRoute('acceuil');
+    // }
 
-    public function showtemplate()
+    // public function showtemplate()
+    // {
+    //     return $this->render('base.html.twig',[]);
+    // }
+
+    #[Route('/products', name: 'products_list')]
+    public function showproducts()
     {
-        return $this->render('base.html.twig',[]);
+       
+        $products = ['ordinateur','telephone','radio','cassette'];
+        return $this->render('product.html.twig', ['products' => $products]);
     }
 }
