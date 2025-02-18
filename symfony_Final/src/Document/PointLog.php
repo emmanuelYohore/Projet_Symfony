282e0d659060d5f28003364a0f8b718f60d07d06
GuillaumeAntier <guillaume.antier@ynov.com>
2025-02-18 15:37:00 +0100

    Gestion de la base de données effectuée et ajout des controlleurs de base

---
+++ symfony_Final/src/Document/PointLog.php
@@ -0,0 +1,42 @@
+<?php
+
+// src/Document/PointsLog.php
+namespace App\Document;
+
+use Doctrine\ODM\MongoDB\Mapping\Annotations as ODM;
+
+/**
+ * @ODM\Document(repositoryClass=PointsLogRepository::class)
+ */
+class PointsLog
+{
+    /**
+     * @ODM\Id(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @ODM\ReferenceOne(targetDocument=User::class)
+     */
+    private $user;
+
+    /**
+     * @ODM\ReferenceOne(targetDocument=Group::class, nullable=true)
+     */
+    private $group;
+
+    /**
+     * @ODM\Field(type="int")
+     */
+    private $pointsChange;
+
+    /**
+     * @ODM\Field(type="string")
+     */
+    private $reason;
+
+    /**
+     * @ODM\Field(type="date")
+     */
+    private $timestamp;
+}
