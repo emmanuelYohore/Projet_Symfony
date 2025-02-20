7e7e73d938580074dad2844d6badaf1116635a26
emmanuel.yohore@ynov.com <emmanuel.yohore@ynov.com>
2025-02-20 16:01:24 +0100

    home contrller

--- symfony_Final/src/Controller/HomeController.php
+++ symfony_Final/src/Controller/HomeController.php
@@ -1,88 +1,54 @@
 <?php
-declare(strict_types=1);
+
 namespace App\Controller;
-use App\Document\Group;
-use App\Document\Invitation;
-use App\Document\User;
-use App\Document\UserHabit;
-use App\Document\PointLog;
+
 use App\Document\Habit;
-use App\Document\HabitCompletion;
-use App\Form\UserType;
+use App\Document\User;
+use App\Document\Group;
 use Doctrine\ODM\MongoDB\DocumentManager;
-use MongoDB\BSON\Regex;
-use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\HttpFoundation\File\Exception\FileException;
 
-class HomeController extends AbstractController {
+class HomeController extends AbstractController
+{
     private DocumentManager $dm;
-    private LoggerInterface $logger;
-    public function __construct(DocumentManager $dm, LoggerInterface $logger)
+
+    public function __construct(DocumentManager $dm)
     {
         $this->dm = $dm;
-        $this->logger = $logger;
     }
 
-    #[Route('/home', name: 'home_index', methods: ['GET', 'POST'])]
-    public function index(Request $request,SessionInterface $session): Response
-    {   
-        $userRepository = $this->dm->getRepository(User::class);
-        $users = $userRepository->findAll();
-        $id = $session->get('connected_user');
-        $user = new User();
-        $form = $this->createForm(UserType::class, $user);
-        $form->handleRequest($request);
-
-        
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $profilePicture = $form->get("profile_picture")->getData();
-
-            if ($profilePicture) {
-                $originalFileName = pathinfo($profilePicture->getClientOriginalName(),PATHINFO_FILENAME);
-                $newFileName = $originalFileName . '-' . uniqid() . '.' . $profilePicture->guessExtension();
-                try {
-                    $profilePicture->move(
-                        $this->getParameter('picture_directory'), // Assure-toi d’avoir ce paramètre configuré dans services.yaml
-                        $newFileName
-                    );
-                } catch (FileException $e) {
-                    $this->addFlash('error', 'Une erreur est survenue lors de l\'upload de l\'image.');
-                    return $this->redirectToRoute('app_register');
-                }
-                
-                $user->setProfilePicture($newFileName);
-            }
-            $user->setPassword(password_hash($user->getPassword(),PASSWORD_BCRYPT));
-            $this->dm->persist($user);
-            $session->set('connected_user',$user->getId());
-            $this->dm->flush();
-            return $this->redirectToRoute('home_index');
-        }
-
-        return $this->render('habitica/home.html.twig', [
-            'users' => $users,
-            'form' => $form->createView(),
-            'user' => $session->get('connected_user') ? $this->dm->getRepository(User::class)->findOneBy(['id' => $session->get('connected_user')]) : null,
-        ]);
-
-
-    }
-
-    #[Route('/habitica-home/delete/{id}', name: 'user_delete', methods: ['POST'])]
-    public function delete(Request $request, string $id): Response
+    #[Route('/home', name: 'home_index')]
+    public function index(SessionInterface $session): Response
     {
-        $user = $this->dm->getRepository(User::class)->find($id);
-        if ($user) {
-            $this->dm->remove($user);
-            $this->dm->flush();
+        $userId = $session->get('connected_user');
+        if (!$userId) {
+            return $this->redirectToRoute('app_login');
         }
 
-        return $this->redirectToRoute('home_index');
+        $user = $this->dm->getRepository(User::class)->findOneBy(['id' => $userId]);
+        if (!$user) {
+            return $this->redirectToRoute('app_logout');
+        }
+
+        $userHabits = $this->dm->getRepository(Habit::class)->findBy(['user' => $user]);
+
+      
+        $groupHabits = [];
+        if ($user->getGroupId()) {
+            $group = $this->dm->getRepository(Group::class)->findOneBy(['id' => $user->getGroupId()]);
+            if ($group) {
+                $groupHabits = $this->dm->getRepository(Habit::class)->findBy(['group' => $group]);
+            }
+        }
+
+        return $this->render('home/index.html.twig', [
+            'user' => $user,
+            'userHabits' => $userHabits,
+            'groupHabits' => $groupHabits
+        ]);
     }
 }
